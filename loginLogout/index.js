23a844f8ab757cf9a5884bcdbda02df502663d83
aditya-docs <aditya@tray.io>
2023-05-06 17:17:29 +0100

    added login route

--- loginLogout/index.js
+++ loginLogout/index.js
@@ -2,6 +2,9 @@ import express from "express";
 import bodyParser from "body-parser";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
+import bcrypt from "bcrypt";
+import User from "./models/userModel.js";
+import jwt from "jsonwebtoken";
 dotenv.config();
 connectDB();
 const app = express();
@@ -11,7 +14,61 @@ app.use(bodyParser.json());
 
 app.get("/test", (req, res) => res.send("API running"));
 
-// app.post('/register', )
+app.post("/register", async (req, res) => {
+  let user;
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    user = new User({
+      email: req.body.email,
+      password: hashedPassword,
+    });
+  } catch (error) {
+    return res.status(400).send({ message: "Bad request" });
+  }
+
+  try {
+    const response = await user.save();
+    res.status(201).send({
+      message: "User created successfully",
+      response,
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: "Error creating user",
+      error,
+    });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return res.status(404).send({
+      message: "Please check your credentials",
+    });
+  }
+  const compareResult = await bcrypt.compare(req.body.password, user.password);
+  if (!compareResult) {
+    return res.status(404).send({
+      message: "Please check your credentials",
+    });
+  }
+  // create JWT token
+  const token = jwt.sign(
+    {
+      userId: user._id,
+      userEmail: user.email,
+    },
+    process.env.SECRET_KEY,
+    { expiresIn: "2m" }
+  );
+
+  res.send({
+    message: "Login successful",
+    email: user.email,
+    token,
+  });
+});
 
 app.listen(PORT, () =>
   console.log(`Server is running on http:localhost:${PORT}`)
